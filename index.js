240440f49c6d557fb2a434d7443c112b7b4d675b
Chris Boette <chris.boette@ownlocal.com>
2016-12-22 16:06:20 -0600

    Do not track npm log. Change style on index.js.

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
 var Botkit = require('botkit');
 
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
-    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
-    process.exit(1);
+  console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+  process.exit(1);
 }
 
+var config = {};
 
-var config = {}
-
-var controller = Botkit.slackbot(config).configureSlackApp(
-    {
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        scopes: ['commands'],
-    }
-);
+var controller = Botkit.slackbot(config).configureSlackApp({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  scopes: ['commands']
+});
 
 controller.setupWebserver(process.env.PORT, function (err, webserver) {
-    controller.createWebhookEndpoints(controller.webserver);
+  controller.createWebhookEndpoints(controller.webserver);
 
-    controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
-        if (err) {
-            res.status(500).send('ERROR: ' + err);
-        } else {
-            res.send('Success!');
-        }
-    });
+  controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+    } else {
+      res.send('Success!');
+    }
+  });
 });
 
 controller.on('slash_command', function (slashCommand, message) {
+  switch (message.command) {
+    case '/snag':
+      if (message.token !== process.env.VERIFICATION_TOKEN) return; // just ignore it.
 
-    switch (message.command) {
-        case "/snag":
-            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+      message.text;
 
-            message.text
+      // if no text was supplied, treat it as a help command
+      if (message.text === '' || message.text === 'help') {
+        slashCommand.replyPrivate(message, "I don't know what I'm doing help!!!");
+        return;
+      }
 
-            // if no text was supplied, treat it as a help command
-            if (message.text === "" || message.text === "help") {
-                slashCommand.replyPrivate(message, "I don't know what I'm doing help!!!");
-                return;
-            }
+      // // If we made it here, just echo what the user typed back at them
+      // //TODO You do it!
+      // slashCommand.replyPublic(message, "1", function() {
+      //     slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+      // });
 
-            // // If we made it here, just echo what the user typed back at them
-            // //TODO You do it!
-            // slashCommand.replyPublic(message, "1", function() {
-            //     slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            // });
-
-            break;
-        default:
-            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
-
-    }
-
+      break;
+    default:
+      slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + ' yet.');
+  }
 });
