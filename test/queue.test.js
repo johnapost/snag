0f47b2f3b8048afdc8da8af1ece2444ce4e79e07
Chris Boette <chris.boette@ownlocal.com>
2016-12-27 19:03:08 -0500

    Add some spacing to the tests.

--- test/queue.test.js
+++ test/queue.test.js
@@ -9,7 +9,8 @@ test.afterEach((t) => {
 
 test('List all of the queue names', (t) => {
   const queueNames = '• af-prod\n• af-staging\n• api-prod\n• api-staging\n• impd-prod\n• impd-staging\n• lf-prod\n• lf-staging\n• mch-prod\n• mch-staging\n• mp-prod\n• mp-staging\n• po-prod\n• po-staging\n';
-  t.deepEqual(queue.listQueues(), queueNames);
+
+  t.is(queue.listQueues(), queueNames);
 });
 
 test('Returns help text', (t) => {
@@ -19,6 +20,7 @@ test('Returns help text', (t) => {
 test('Claim an open resource', (t) => {
   const resource = 'af-prod';
   const username = 'X';
+
   t.is(queue.claim(resource, username), `AF Prod is yours, X. Don't forget to release it with \`/snaggg release ${resource}\` when you're finished.`);
 });
 
@@ -33,12 +35,14 @@ test('Format the resource name', (t) => {
 test('Resource name is in the store', (t) => {
   const resource = 'af-prod';
   const store = {'af-prod': undefined};
+
   t.true(queue.inStore(store, resource));
 });
 
 test('Resource name is not in the store', (t) => {
   const resource = 'af-pr00d';
   const store = {'af-prod': undefined};
+
   t.false(queue.inStore(store, resource));
 });
 
@@ -46,6 +50,7 @@ test('Add a username to the resource store', (t) => {
   const store = {'af-prod': undefined};
   const username = 'X';
   const resource = 'af-prod';
+
   t.is(queue.assignResource(store, username, resource)[resource], username);
 });
 
@@ -53,6 +58,7 @@ test('Returns a message when resource store is spelled incorrectly', (t) => {
   const store = {'af-prod': undefined};
   const username = 'X';
   const resource = 'af-prdo';
+
   t.is(queue.assignResource(store, username, resource), 'Sorry, X, I don\'t know what AF Prdo is.');
 });
 
@@ -60,6 +66,7 @@ test('Returns a message when resource store is already claimed', (t) => {
   const store = {'af-prod': 'Y'};
   const username = 'X';
   const resource = 'af-prod';
+
   t.is(queue.assignResource(store, username, resource), 'Sorry, X, AF Prod is currently claimed by Y.');
 });
 
@@ -67,6 +74,7 @@ test('Releases the specified resource', (t) => {
   const store = {'af-prod': 'X'};
   const username = 'X';
   const resource = 'af-prod';
+
   t.falsy(queue.releaseResource(store, username, resource)[resource]);
 });
 
@@ -74,6 +82,7 @@ test('Releases all resources claimed by the user', (t) => {
   const store = {'af-prod': 'X', 'mp-prod': 'X', 'po-staging': 'Z'};
   const username = 'X';
   const updatedStore = queue.releaseResource(store, username);
+
   t.false(_(updatedStore).values().includes(username));
 });
 
@@ -93,6 +102,7 @@ test('List all claimed resources', (t) => {
   const username = 'X';
   const resource = 'af-prod';
   queue.assignResource(queue.store, username, resource);
+
   t.is(queue.all(), 'AF Prod: X\n');
 });
 
